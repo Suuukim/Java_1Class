66cf306223a09ed75e91ec5f53573ed0a1ac8342
Suuukim <gidrl@DESKTOP-9FCDD9U>
2022-11-04 23:26:14 +0900

    day1

--- src/testpackage/FirstClass.java
+++ src/testpackage/FirstClass.java
@@ -7,9 +7,12 @@ public class FirstClass {
 		/* 
 		 * 여러줄
 		 * 주석입니다. 
+		 * System.out.println("Hello World");
 		 */
 		
 		//ctrl + spacebar : 자동완성
-		System.out.println("Hellp World");
+		System.out.println("Hello World");
+		//실행 : ctrl + F11
+		//한줄 삭제 : ctrl + D
 	}
 }
