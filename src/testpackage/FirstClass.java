1fcdec5bf04608c1fa752ee2c481861ee77c18d3
Suuukim <gidrl@DESKTOP-9FCDD9U>
2022-11-04 22:26:09 +0900

    day1

---
+++ src/testpackage/FirstClass.java
@@ -0,0 +1,15 @@
+package testpackage;
+
+public class FirstClass {
+	public static void main(String[] args) {
+		// 주석입니다. 
+		
+		/* 
+		 * 여러줄
+		 * 주석입니다. 
+		 */
+		
+		//ctrl + spacebar : 자동완성
+		System.out.println("Hellp World");
+	}
+}
