6d92c6272d0b2cefb525c231db945206130d3e9b
Suuukim <toki159@naver.com>
2023-06-23 22:14:40 +0900

    자바의 정석

---
+++ ch02/src/defalt/VarEx1.java
@@ -0,0 +1,12 @@
+package defalt;
+
+public class VarEx1 {
+	public static void main(String[] args) {
+		int x = 4, y = 2;
+		System.out.println(x+y);	// µ¡¼À
+		System.out.println(x-y);	// »¬¼À
+		System.out.println(x*y);	// °ö¼À
+		System.out.println(x/y);	// ³ª´°¼À
+		
+	}
+}
