6bdd74b5f52ffba8e508bd2efbf556e58cc8183f
Suuukim <toki159@naver.com>
2023-07-12 21:43:00 +0900

    random

---
+++ 09day/src/CountTest.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class CountTest {
+	public static void main(String[] args) {
+		
+		int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0;
+		
+		Random random = new Random();
+		for(int i=1;i<10;i++) {	
+			int r = random.nextInt() + 1;
+		}
+		
+	}
+}
