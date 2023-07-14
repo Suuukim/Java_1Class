c8f063274ba23ff3d2ddcc1d2de7bb8eee86949a
Suuukim <toki159@naver.com>
2023-07-14 21:31:28 +0900

    do while"

---
+++ 11day/src/WhileTest.java
@@ -0,0 +1,29 @@
+
+public class WhileTest {
+	public static void main(String[] args) {
+		
+//		while(조건식) {
+//		조건이 참인 동안 실행할 문장;
+//		....;
+//		}
+		
+		int sum = 0, i = 0;
+		while(i < 100) {
+			sum += ++i;
+		}
+		System.out.println("1~100의 합계 : " + sum);
+		
+//		do {
+//			조건이 참인 동안 실행할 문장;
+//		
+//		} while(조건식);		//";"을 빼먹으면 안된다.
+		
+		sum = i = 0;		// 변수를 선언할 떄는 사용할 수 없다.
+		do {
+			sum += ++i;
+		}while(i < 100);
+		System.out.println("1~100의 합계 : " + sum);
+		
+		
+	}
+}
