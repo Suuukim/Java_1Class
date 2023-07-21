8cf921d6c322522fba7b798e169d6533d8ed529b
Suuukim <toki159@naver.com>
2023-07-21 21:10:31 +0900

    메소드

--- 16day/src/MainClass.java
+++ 16day/src/MainClass.java
@@ -1,4 +1,13 @@
 
 public class MainClass {
+	public static void main(String[] args) {
+		
+		System.out.println(Math.sqrt(2));
+		
+		
+		MethodClass methodClass = new MethodClass();
+		System.out.println("1~100의 합계 : " + methodClass.total());
 
+		System.out.println("1~100의 합계 : " + MethodClass.total());
+	}
 }
